2a17d4c6009f111170fade866318475983702d8b
karimboyevaqunduz544-netizen <karimboyevaqunduz544@gmail.com>
2026-06-20 11:55:28 +0500

    Create Vazifalar.py

---
+++ Vazifalar.py
@@ -0,0 +1,57 @@
+
+print("1-dars Pythonda print funksiyasi")
+print('Assalom alaykum')
+print("Hayrli tong!")
+print(2+4*2)
+print(19/5)
+print(2**4)
+print("2-dars print(),sintexva arifmetik amallar")
+#matnni chiqaring
+print("Nexia,'Tico','Damas' ko'rganlar qilar havas")
+#5ning 4-darajasini toping
+print(5**4)
+#22ni 4ga bo'lganda qancha qoldiq qoladi?
+print(22%4)
+#Tomonlari 125ga teng kvadratning yuzi va perimetrini  toping
+a = b = 125
+print("S= " ,a*a)
+print("P= " ,4*a)
+#diametri 12ga teng bolgan doiraning yuzini toping
+d=12
+r=d/2
+print("S = " , 3.14 * r**2)
+
+#Katetlari 6 va 7 bo'lgan togri burchakli uchburchakning gipotenuzasini toping(Pifagor teoremasidan qarab)
+a = 6
+b = 7
+c = (a**2 + b**2) ** 0.5
+print("Gipotenuza c =", c)
+"Sariq Dev (Anvar Narzullayev) darslari asosida bajarilgan amaliy vazifala
+print("1-dars Pythonda print funksiyasi")
+print('Assalom alaykum')
+print("Hayrli tong!")
+print(2+4*2)
+print(19/5)
+print(2**4)
+print("2-dars print(),sintexva arifmetik amallar")
+#matnni chiqaring
+print("Nexia,'Tico','Damas' ko'rganlar qilar havas")
+#5ning 4-darajasini toping
+print(5**4)
+#22ni 4ga bo'lganda qancha qoldiq qoladi?
+print(22%4)
+#Tomonlari 125ga teng kvadratning yuzi va perimetrini  toping
+a = b = 125
+print("S= " ,a*a)
+print("P= " ,4*a)
+#diametri 12ga teng bolgan doiraning yuzini toping
+d=12
+r=d/2
+print("S = " , 3.14 * r**2)
+
+#Katetlari 6 va 7 bo'lgan togri burchakli uchburchakning gipotenuzasini toping(Pifagor teoremasidan qarab)
+a = 6
+b = 7
+c = (a**2 + b**2) ** 0.5
+print("Gipotenuza c =", c)
+"Sariq Dev (Anvar Narzullayev) darslari asosida bajarilgan amaliy vazifalar."r."

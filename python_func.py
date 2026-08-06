6557b188464c31c17e86a055f58ff86084637e05
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-07 00:39:00 +0500

    Create python_func.py

---
+++ python_func.py
@@ -0,0 +1,26 @@
+"""
+IMPORTANT
+PYTHON
+FUNCTIONS
+"""
+#1! print() (chop etish,chiqarish funksiyasi)
+print("Hello , World!") #Hello,World!
+#2! input() (foydalanuvchidan qiymat kiritishini soraymiz )
+name = input("Enter your name:") #Enter your name: Qunduz
+#3! len() (string,tuple,list va hk uzunligini aniqlaymiz)
+print(len([1,2,3,4]))#4
+#4! type() (ma'lumot turini aniqlash :int,str, float, hk)
+print(type(5)) #<class 'int'>
+#5! range() (sanash operatori)
+for i  in range(5):
+	pass#vaxtinchalik joy egallovchi
+#6! int() (integer haqiqiy  son)
+num = int("10")
+#7!  float() (butun son)
+num = float("10.5")
+#8! str()(matn)
+num = str(10)
+#9! sum() Ro'yxatdagi barcha sonlar yigindisini hisoblaydi
+print(sum([1,2,3])) #6
+#10! max() Eng katta qiymatni qaytaradi
+print(max([10,20,30])) #30

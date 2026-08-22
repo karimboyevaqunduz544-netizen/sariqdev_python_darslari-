9b66d46e9b33083cd8f1d2d3218c3cbf4fd550cd
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-22 07:53:47 +0500

    Create shart_oper.py

---
+++ shart_oper.py
@@ -0,0 +1,21 @@
+# 1! If sharti
+age = 56
+if age > 10:
+		print("Not Child") #Not Child
+#2! If else
+age = 9
+if age > 10:
+			print("Not Child") 
+else:
+			print("Child") # Child
+#3! If elif else
+age =43
+if age > 75:
+			print("Very Old")
+elif age > 45:
+			print("Old")
+elif age > 15:
+		print("Young")
+else:
+		print("Child")
+		#Young

4e65072ee6fcddab83dfcf71df7975d53e262181
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-09-23 05:45:56 +0500

    Implement sum function to add three user inputs

---
+++ def_sum.py
@@ -0,0 +1,15 @@
+def sum():
+	  a = int(input( "Enter your number:  "))
+	  b = int(input("Enter your number: "))
+	  c = int(input("Enter your number: "))
+	  sum = a+b+c
+	  print(sum)
+sum()
+
+"""
+Enter your number:  5
+Enter your number: 3
+Enter your number: 4
+12
+
+"""
